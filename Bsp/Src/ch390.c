fe239345df7121deef8ee46f1d71d0bd5c7b50e2
lzt404 <2596933790@qq.com>
2025-04-29 12:32:05 +0800

    单网卡初代

---
+++ Bsp/Src/ch390.c
@@ -0,0 +1,369 @@
+#include "ch390.h"
+
+/**
+ * @name ch390_get_link_status
+ * @brief Get link status of the internal PHY
+ * @return 0: Link failed  1: Link OK
+ */
+int ch390_get_link_status()
+{
+    uint8_t nsr = ch390_read_reg(CH390_NSR);
+    return !!(nsr & NSR_LINKST);
+}
+
+/**
+ * @name ch390_get_int_status
+ * @brief Get CH390 interrupt status and clear them
+ * @return Interrupt status
+ */
+uint8_t ch390_get_int_status()
+{
+    uint8_t int_status = ch390_read_reg(CH390_ISR);
+    // Clear interrupt status by write 1
+    ch390_write_reg(CH390_ISR, int_status);
+    return int_status;
+}
+
+/**
+ * @name ch390_write_reg
+ * @brief Write register (HAL库版本)
+ * @param reg - Target register address
+ * @param value - Value to be written
+ */
+void ch390_write_reg(uint8_t reg, uint8_t value)
+{
+    uint8_t buf[2];
+    buf[0] = reg | OPC_REG_W;
+    buf[1] = value;
+
+    ch390_cs(0); // CS = 0
+    HAL_SPI_Transmit(&hspi2, buf, 2, 200);
+    ch390_cs(1); // CS = 1
+}
+
+void ch390_delay_us(uint32_t us)
+{
+    uint32_t start = DWT->CYCCNT;
+    uint32_t ticks = us * (SystemCoreClock / 1000000);
+    while ((DWT->CYCCNT - start) < ticks);
+}
+
+/**
+ * @name ch390_software_reset
+ * @brief Software reset CH390 by NCR
+ */
+void ch390_software_reset()
+{
+    ch390_write_reg(CH390_NCR, NCR_RST);
+    ch390_delay_us(10);
+    ch390_write_reg(CH390_NCR, 0);
+    ch390_write_reg(CH390_NCR, NCR_RST);
+    ch390_delay_us(10);
+}
+
+// PHY registers
+#define CH390_PHY          0x40
+#define CH390_PHY_BMCR     0x00
+#define CH390_PHY_BMSR     0x01
+#define CH390_PHY_PHYID1   0x02
+#define CH390_PHY_PHYID2   0x03
+#define CH390_PHY_ANAR     0x04
+#define CH390_PHY_ANLPAR   0x05
+#define CH390_PHY_ANER     0x06
+#define CH390_PHY_PAGE_SEL 0x1F
+
+// Packet status
+#define CH390_PKT_NONE  0x00    /* No packet received */
+#define CH390_PKT_RDY   0x01    /* Packet ready to receive */
+#define CH390_PKT_ERR   0xFE    /* Un-stable states */
+#define CH390_PKT_MAX   1536    /* Received packet max size */
+#define CH390_PKT_MIN   64
+
+static uint8_t ch390_spi_exchange_byte(uint8_t byte)
+{
+    uint8_t rx = 0;
+    HAL_SPI_TransmitReceive(&hspi2, &byte, &rx, 1,200);
+    return rx;
+}
+#define ch390_spi_dummy_read() ch390_spi_exchange_byte(0)
+
+/**
+ * @name ch390_read_reg
+ * @brief Read register
+ * @param reg - Target register address
+ * @return Register value
+ */
+uint8_t ch390_read_reg(uint8_t reg)
+{
+    uint8_t spi_data;
+    ch390_cs(0); // CS = 0
+    ch390_spi_exchange_byte(reg | OPC_REG_R);
+    spi_data = ch390_spi_dummy_read();
+    ch390_cs(1); // CS = 1
+    return spi_data;
+}
+
+/**
+ * @name ch390_get_vendor_id
+ * @brief Get vendor ID
+ * @return Vendor ID
+ */
+uint16_t ch390_get_vendor_id()
+{
+    uint16_t id;
+    id = (ch390_read_reg(CH390_VIDL) & 0xff);
+    id |= ch390_read_reg(CH390_VIDH) << 8;
+    return id;
+}
+
+/**
+ * @name ch390_get_product_id
+ * @brief Get product ID
+ * @return Product ID
+ */
+uint16_t ch390_get_product_id()
+{
+    uint16_t id;
+    id = (ch390_read_reg(CH390_PIDL) & 0xff);
+    id |= ch390_read_reg(CH390_PIDH) << 8;
+    return id;
+}
+
+/**
+ * @name ch390_get_mac
+ * @brief Get mac address
+ * @param mac_addr - 6-byte length mac address output
+ */
+void ch390_get_mac(uint8_t *mac_addr)
+{
+    uint8_t i;
+    for (i = 0; i < 6; i++)
+    {
+        mac_addr[i] = ch390_read_reg(CH390_PAR + i);
+    }
+    ch390_read_reg(0);
+}
+
+/**
+ * @name ch390_write_phy
+ * @brief Write PHY register
+ * @param reg - PHY register address
+ * @param value - Value to be written
+ */
+void ch390_write_phy(uint8_t reg, uint16_t value)
+{
+    ch390_write_reg(CH390_EPAR, CH390_PHY | reg);
+    ch390_write_reg(CH390_EPDRL, (value & 0xff));        // Low byte
+    ch390_write_reg(CH390_EPDRH, ((value >> 8) & 0xff)); // High byte
+    // Chose PHY, send write command
+    ch390_write_reg(CH390_EPCR, 0x0A);
+    while(ch390_read_reg(CH390_EPCR) & 0x01);
+    // Clear write command
+    ch390_write_reg(CH390_EPCR, 0x00);
+}
+
+/**
+ * @name ch390_set_phy_mode
+ * @brief Set PHY mode and enable PHY.
+ *        PHY mode: Auto-negotiation, 10M/100M, full-duplex/half-duplex
+ * @param mode - PHY mode
+ */
+void ch390_set_phy_mode(enum ch390_phy_mode mode)
+{
+    uint16_t BMCR_value = 0;
+    uint16_t ANAR_value = 0;
+    switch (mode)
+    {
+    case CH390_10MFD:
+        BMCR_value = 0x1100;
+        ANAR_value = 0x41;
+        break;
+    case CH390_100MFD:
+        BMCR_value = 0x3100;
+        ANAR_value = 0x101;
+        break;
+    case CH390_AUTO:
+        BMCR_value = 0x1000;
+        ANAR_value = 0x01E1;
+        break;
+    }
+    ch390_write_phy(CH390_PHY_BMCR, BMCR_value);
+    ch390_write_phy(CH390_PHY_ANAR, ANAR_value);
+    ch390_write_reg(CH390_GPR, 0x00);   // Enable PHY
+}
+
+/**
+ * @name ch390_set_multicast
+ * @brief Set multicast address hash table
+ * @param multicast_addr - 8-byte length multicast address hash table array
+ */
+void ch390_set_multicast(uint8_t *multicast_hash)
+{
+    uint8_t i;
+    for (i = 0; i < 8; i++)
+    {
+        ch390_write_reg(CH390_MAR + i, multicast_hash[i]);
+    }
+    ch390_read_reg(0);
+}
+
+
+/**
+ * @name ch390_default_config
+ * @brief Config CH390 with default options:
+ *        LED mode 1;
+ *        Enable transmit check sum generation;
+ *        Enable RX;
+ *        Enable all interrupt and PAR
+ */
+void ch390_default_config()
+{
+    // CH390 has built-in MAC, this is not necessary
+    // uint8_t mac_addr[6] = { 0x50, 0x54, 0x7B, 0x84, 0x00, 0x73 };
+    // Multicast address hash table
+    uint8_t multicase_addr[8] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+    ch390_set_phy_mode(CH390_AUTO);
+    // Clear status
+    ch390_write_reg(CH390_NSR, NSR_WAKEST | NSR_TX2END | NSR_TX1END);
+    ch390_write_reg(CH390_ISR, 0xFF);         // Clear interrupt status
+    ch390_write_reg(CH390_TCR2, 0x80);        // LED mode 1
+    ch390_write_reg(CH390_TCSCR, TCSCR_ALL);  // Enable check sum generation
+
+    // ch390_set_mac_address(mac_addr);
+    ch390_set_multicast(multicase_addr);
+
+    // Enable all interrupt and PAR
+    ch390_write_reg(CH390_IMR, IMR_ALL);
+    // Enable RX
+    ch390_write_reg(CH390_RCR, RCR_DIS_CRC | RCR_RXEN);
+}
+
+/**
+ * @name ch390_write_mem
+ * @brief Write data to TX SRAM
+ * @param data - Data buffer
+ * @param length - Length to write
+ */
+void ch390_write_mem(uint8_t *data, int length)
+{
+    int i;
+    ch390_cs(0);
+    ch390_spi_exchange_byte(OPC_MEM_WRITE);
+    for (i = 0; i < length; i++)
+    {
+        ch390_spi_exchange_byte(data[i]);
+    }
+    ch390_cs(1);
+}
+
+/**
+ * @name ch390_send_request
+ * @brief Issue transmit request
+ */
+void ch390_send_request()
+{
+    uint8_t tcr = ch390_read_reg(CH390_TCR);
+    ch390_write_reg(CH390_TCR, tcr | TCR_TXREQ);
+}
+
+/**
+ * @name ch390_send_packet
+ * @brief Send packet
+ * @param buff - Data to be sent
+ * @param length - Less than 3k bytes.
+ */
+void ch390_send_packet(uint8_t *buff, uint16_t length)
+{
+    // Write data to SRAM
+    ch390_write_mem(buff, length);
+    // Wait until last transmit complete
+    while(ch390_read_reg(CH390_TCR) & TCR_TXREQ);
+    // Set current packet length
+    ch390_write_reg(CH390_TXPLL, length & 0xff);
+    ch390_write_reg(CH390_TXPLH, (length >> 8) & 0xff);
+    // Issue transmit request
+    ch390_send_request();
+}
+
+/**
+ * @name ch390_get_int_pin
+ * @brief Get ch390 interrupt signal (read PC4)
+ */
+uint16_t ch390_get_int_pin(void)
+{
+    //return GPIOC->IDR & GPIO_PIN_4;
+    return HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_4);
+}
+
+/**
+ * @name ch390_read_mem
+ * @brief Read data from RX SRAM
+ * @param data - Data buffer
+ * @param length - Length to read
+ */
+void ch390_read_mem(uint8_t *data, int length)
+{
+    int i;
+    ch390_cs(0);
+    ch390_spi_exchange_byte(OPC_MEM_READ);
+
+    for (i = 0; i < length; i++)
+    {
+        data[i] = ch390_spi_dummy_read();
+    }
+    ch390_cs(1);
+}
+
+/**
+ * @name ch390_receive_packet
+ * @brief Receive packet
+ * @param buff - Size equal to CH390_PKT_MAX
+ * @param rx_status - Output abnormal status while receiving packet.
+ *                    It has the same meaning as RSR(06h).
+ * @return Packet length
+ */
+uint32_t ch390_receive_packet(uint8_t *buff, uint8_t *rx_status)
+{
+    uint8_t rx_ready;
+    uint16_t rx_len = 0;
+	uint8_t ReceiveData[4];
+
+    // Check packet ready or not
+    ch390_read_reg(CH390_MRCMDX);
+    rx_ready = ch390_read_reg(CH390_MRCMDX);
+
+    // if rxbyte != 1 or 0 reset device
+    if (rx_ready & CH390_PKT_ERR)
+    {
+        // Reset RX FIFO pointer
+        uint8_t rcr = ch390_read_reg(CH390_RCR);
+        ch390_write_reg(CH390_RCR, rcr & ~RCR_RXEN); //RX disable
+        ch390_write_reg(CH390_MPTRCR, 0x01);  //Reset RX FIFO pointer
+        ch390_write_reg(CH390_MRRH, 0x0c);
+        ch390_delay_us(1000);
+        ch390_write_reg(CH390_RCR, rcr | RCR_RXEN); //RX Enable
+        return 0;
+    }
+    if (!(rx_ready & CH390_PKT_RDY))
+    {
+        return 0;
+    }
+
+    ch390_read_mem(ReceiveData, 4);
+
+    *rx_status = ReceiveData[1];
+    rx_len = ReceiveData[2] | (ReceiveData[3] << 8);
+
+    if(rx_len <= CH390_PKT_MAX)
+    {
+        ch390_read_mem(buff, rx_len);
+    }
+
+    if ((*rx_status & 0x3f) || (rx_len > CH390_PKT_MAX))
+    {
+        return 0;
+    }
+    return rx_len;
+}
+
---
+++ Core/Src/main.c
@@ -0,0 +1,321 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "spi.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "ch390.h"
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+#define xprintf(fmt, ...) \
+    do { \
+        snprintf((char*)uart1_tx_buf, sizeof(uart1_tx_buf), fmt, ##__VA_ARGS__); \
+        HAL_UART_Transmit(&huart1, uart1_tx_buf, strlen((char*)uart1_tx_buf), 5000); \
+    } while(0)
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+uint8_t uart1_tx_buf[128];
+// phy link status
+uint8_t phy_linked = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+void dwt_delay_init(void)
+{
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk; // 使能DWT
+    DWT->CYCCNT = 0;                                // 复位计数器
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;            // 使能计数
+}
+
+/**
+ * @name ch390_print_info
+ * @brief Read and print chip's ID and MAC address
+ */
+void ch390_print_info()
+{
+  uint8_t i;
+  uint8_t mac_addr[6];
+  uint16_t vid = ch390_get_vendor_id();
+  uint16_t pid = ch390_get_product_id();
+  xprintf("ID: %04x%04x\r\n", vid, pid);
+
+  ch390_get_mac(mac_addr);
+  xprintf("MAC: ");
+  for (i = 0; i < 6; i++)
+  {
+      xprintf("%02X ", mac_addr[i]);
+  }
+  xprintf("\r\n");
+}
+
+// Random packet data for send test
+#define TEST_DATA_LEN 600
+#define CH390_PKT_MAX   1536    /* Received packet max size */
+uint8_t packet_data[TEST_DATA_LEN];
+uint8_t receive_buff[CH390_PKT_MAX];
+void init_packet_data()
+{
+    int i;
+    for(i = 0; i < TEST_DATA_LEN; i++)
+    {
+        packet_data[i] = i;
+    }
+}
+
+void print_packet(uint8_t *buff, uint16_t length)
+{
+		int i;
+    xprintf("------------------\r\n"
+           "Packet length: %d", length);
+
+    for(i = 0; i < length; i++)
+    {
+        if(i % 16 == 0)
+        {
+            xprintf("\r\n%04x   ", i);
+        }
+        xprintf("%02x ", buff[i]);
+    }
+
+    xprintf("\r\n");
+}
+
+/**
+ * @name ch390_int_handler
+ * @brief Handle CH390 interrupt events, include packet receive
+ */
+void ch390_int_handler()
+{
+  uint8_t int_status = ch390_get_int_status();
+  // Link status changed
+  if(int_status & ISR_LNKCHG)
+  {
+    HAL_Delay(65);
+    phy_linked = ch390_get_link_status();
+    xprintf("Link status: %d\r\n", phy_linked);
+    ch390_write_reg(CH390_ISR, ISR_LNKCHG);
+  }
+  // Receive overflow
+  if(int_status & ISR_ROS) 
+  {
+    xprintf("Receive overflow\r\n");
+    uint8_t rx_status = 0;
+    uint32_t length = 0;
+    while((length = ch390_receive_packet(receive_buff, &rx_status)) != 0)
+    {
+      // printf("Rx: %d\r\n", length);
+      print_packet(receive_buff, length);
+    }
+  }
+  // Receive overflow counter overflow
+  if(int_status & ISR_ROO) xprintf("Overflow counter overflow\r\n");
+  // Packet transmitted
+//  if(int_status & ISR_PT) printf("Packet sent\r\n");
+  // Packet received
+  if(int_status & ISR_PR)
+  {
+      /* Multiple packets may be received in a single packet receive
+       * event. So "ch390_receive_packet" should be called until the
+       * return value is 0. Otherwise data will accumulate in RX SRAM
+       * and cause overflow. */
+      uint8_t rx_status = 0;
+      uint32_t length = 0;
+      while((length = ch390_receive_packet(receive_buff, &rx_status)) != 0)
+      {
+          /* Notice, the last 4 bytes of the received data are CRC */
+          // printf("Rx: %d\r\n", length);
+          print_packet(receive_buff, length);
+      }
+  }
+}
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  dwt_delay_init();
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+  ch390_software_reset();
+  HAL_Delay(10);
+  ch390_default_config();
+  ch390_print_info();
+  init_packet_data(); 
+
+  //ch390_send_packet(packet_data, TEST_DATA_LEN);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if(ch390_get_int_pin())
+    {
+      ch390_int_handler();
+    }
+
+    if(phy_linked)
+    {
+  //    xprintf("ok o\n");
+      ch390_send_packet(packet_data, TEST_DATA_LEN);
+    }
+    
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
